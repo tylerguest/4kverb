865acaa3ae8542be462b461797bdf1b010d9e3d3
Tyler Guest <83128087+tylerguest@users.noreply.github.com>
2024-12-07 20:39:41 -0500

    updated decay param range

--- 4kverb/Source/PluginProcessor.cpp
+++ 4kverb/Source/PluginProcessor.cpp
@@ -15,7 +15,7 @@ _4kverbAudioProcessor::_4kverbAudioProcessor()
                  {
                      std::make_unique<juce::AudioParameterFloat>("predelay", "PreDelay", 0.0f, 100.0f, 20.0f),
                      std::make_unique<juce::AudioParameterFloat>("mix", "Mix", 0.0f, 1.0f, 1.0f),
-                     std::make_unique<juce::AudioParameterFloat>("decay", "Decay", 0.0f, 1.0f, 0.5f),
+                     std::make_unique<juce::AudioParameterFloat>("decay", "Decay", 0.2f, 70.0f, 4.0f),
                      std::make_unique<juce::AudioParameterFloat>("size", "Size", 0.0f, 1.0f, 0.5f),
                      std::make_unique<juce::AudioParameterFloat>("highCut", "High Cut", 0.0f, 1.0f, 0.5f),
                      std::make_unique<juce::AudioParameterFloat>("lowCut", "Low Cut", 0.0f, 1.0f, 0.5f)
